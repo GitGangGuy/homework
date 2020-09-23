425a53599a3a1d5f1bae6a3ff74f1d2520068374
GitGangGuy <github@dothomic.de>
2020-09-23 07:32:32 +0000

    feat: broken linkedlist implementation

---
+++ linkedlist/List.java
@@ -0,0 +1,16 @@
+package linkedlist;
+
+import java.utils.Arrays.copyOf;
+
+final public class List<T> {
+    private T[] objects;
+
+    public List() {
+        objects = new T[0];
+    }
+
+    public void add(T item) {
+        objects = objects.copyOf()
+        objects = new T[objects.length];
+    }
+}
